26a31ebe53acf430c1e3a97a4d83ff34eac5bc60
chickenbobbobba <sambosh2579@gmail.com>
2025-12-09 13:54:53 +0000

    working braille printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include <algorithm>
+#include <codecvt>
 #include <iostream>
+#include <ostream>
+#include <string>
 #include <vector>
 #include <stdlib.h>
 #include <string.h>
@@ -14,6 +18,7 @@ struct Pixel {
 class Grid {
 public:
     std::vector<std::vector<Pixel>> grid;
+    long long step = 0;
     size_t width = 10;
     size_t height = 5;
 
@@ -39,27 +44,48 @@ public:
     }
 
     void printBraille() {
-        for (size_t i = 0; i < width-1; i++) std::cout << "--";
+        std::string out;
+        out += std::to_string(step);
+        for (size_t i = 0; i < width/2 - 1 - std::to_string(step).size(); i++) out.append("-");
         std::cout << "-\n";
         for (size_t i = 0; i < height; i += 4) {
             for (size_t j = 0; j < width; j += 2) {
+                /*
+                14
+                25
+                36
+                78
+                */
+                long base = 0;
+                
+                base = base | grid[j+0][i+0].state << 0;
+                base = base | grid[j+0][i+1].state << 1;
+                base = base | grid[j+0][i+2].state << 2;
+                base = base | grid[j+1][i+0].state << 3;
+                base = base | grid[j+1][i+1].state << 4;
+                base = base | grid[j+1][i+2].state << 5;
+                base = base | grid[j+0][i+3].state << 6;
+                base = base | grid[j+1][i+3].state << 7;
 
+                std::vector<std::string> braille = {"⠀","⠁","⠂","⠃","⠄","⠅","⠆","⠇","⠈","⠉","⠊","⠋","⠌","⠍","⠎","⠏","⠐","⠑","⠒","⠓","⠔","⠕","⠖","⠗","⠘","⠙","⠚","⠛","⠜","⠝","⠞","⠟","⠠","⠡","⠢","⠣","⠤","⠥","⠦","⠧","⠨","⠩","⠪","⠫","⠬","⠭","⠮","⠯","⠰","⠱","⠲","⠳","⠴","⠵","⠶","⠷","⠸","⠹","⠺","⠻","⠼","⠽","⠾","⠿","⡀","⡁","⡂","⡃","⡄","⡅","⡆","⡇","⡈","⡉","⡊","⡋","⡌","⡍","⡎","⡏","⡐","⡑","⡒","⡓","⡔","⡕","⡖","⡗","⡘","⡙","⡚","⡛","⡜","⡝","⡞","⡟","⡠","⡡","⡢","⡣","⡤","⡥","⡦","⡧","⡨","⡩","⡪","⡫","⡬","⡭","⡮","⡯","⡰","⡱","⡲","⡳","⡴","⡵","⡶","⡷","⡸","⡹","⡺","⡻","⡼","⡽","⡾","⡿","⢀","⢁","⢂","⢃","⢄","⢅","⢆","⢇","⢈","⢉","⢊","⢋","⢌","⢍","⢎","⢏","⢐","⢑","⢒","⢓","⢔","⢕","⢖","⢗","⢘","⢙","⢚","⢛","⢜","⢝","⢞","⢟","⢠","⢡","⢢","⢣","⢤","⢥","⢦","⢧","⢨","⢩","⢪","⢫","⢬","⢭","⢮","⢯","⢰","⢱","⢲","⢳","⢴","⢵","⢶","⢷","⢸","⢹","⢺","⢻","⢼","⢽","⢾","⢿","⣀","⣁","⣂","⣃","⣄","⣅","⣆","⣇","⣈","⣉","⣊","⣋","⣌","⣍","⣎","⣏","⣐","⣑","⣒","⣓","⣔","⣕","⣖","⣗","⣘","⣙","⣚","⣛","⣜","⣝","⣞","⣟","⣠","⣡","⣢","⣣","⣤","⣥","⣦","⣧","⣨","⣩","⣪","⣫","⣬","⣭","⣮","⣯","⣰","⣱","⣲","⣳","⣴","⣵","⣶","⣷","⣸","⣹","⣺","⣻","⣼","⣽","⣾","⣿"};
+                long idx = base & 0xFF;
+                out.append(braille[idx]);
             }
-            std::cout << "\n";
+            out.append("\n");
         }
+        std::cout << "\033[1;1H" << out << std::flush;
     }
 };
 
 int main(int, char**){
     srand(time(NULL));
 
-    Grid grid(100, 20);
-    grid.print();
-    Grid temp(100, 20);
+    Grid grid(340 * 2, 90 * 4);
+    Grid temp = grid;
 
     for (size_t j = 0; j < grid.height; j++) {
         for (size_t i = 0; i < grid.width; i++) {
-            grid.grid[i][j].state = (rand() % 2 == 0);
+            grid.grid[i][j].age = (rand() % 100);
         }
     }
 
@@ -100,15 +126,15 @@ int main(int, char**){
         for (size_t j = 0; j < grid.height; j++) {
             for (size_t i = 0; i < grid.width; i++) {
                 grid.grid[i][j] = temp.grid[i][j];
-                if (grid.grid[i][j].age > 100) {
+                if (grid.grid[i][j].age > (rand() % 200) + 100) {
                     grid.grid[i][j].age = -1;
                 }
 
             }
         }
-        
+        grid.step++;
         grid.printBraille();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     }
 
